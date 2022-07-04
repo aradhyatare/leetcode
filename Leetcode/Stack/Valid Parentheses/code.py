5e66f594c9b209ef7cff2493b6bad095cc2bd64e
Aradhya_Avellino <aradhyatare@gmail.com>
2022-07-04 16:31:48 +0530

    Stack: Min Stack is added

--- Leetcode/Stack/Valid Parentheses/code.py
+++ Leetcode/Stack/Valid Parentheses/code.py
@@ -1,38 +1,61 @@
-# class Solution:
-#     def isValid(self, s: str) -> bool:
-#         list_ = list(s)
-#         mid_l, mid_r = len(list_)/2 - 1, len(list_)/2 + 1
-#         print(mid_l)
-#         print(mid_r)
-#         pass
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pattern = {"{": "}", "(": ")", "[": "]"}
+        stack = []
+        
+        for char in s:
+            if char in pattern:
+                stack.append(char)
+            else:
+                if len(stack) == 0 or pattern[stack.pop()] != char:
+                    return False
+        return len(stack) == 0
 
-s = "()[]{}{}"
-bracket_dict = {'(':')', '[': ']','{':'}'}
-list_ = list(s)
+# s = "([(]){}{})"
+# bracket_dict = {'(':')', '[': ']','{':'}'}
+# list_ = list(s)
+# # mid_l, mid_r = len(list_)//2 - 1, len(list_)//2
+# # print(list_[mid_l], mid_l)
+# # print(list_[mid_r], mid_r)
+# # print(list(s))
 # mid_l, mid_r = len(list_)//2 - 1, len(list_)//2
-# print(list_[mid_l], mid_l)
-# print(list_[mid_r], mid_r)
-# print(list(s))
-mid_l, mid_r = 0
-while mid_l and mid_r in range(len(list_)):
-    # for bracket in list_:
-    mid_l, mid_r = len(list_)//2 - 1, len(list_)//2
-
-    left_bracket = list_[mid_l]
-    r_bracket = list_[mid_r]
-    right_bracket = bracket_dict.get(left_bracket)
 
 
-    if r_bracket == right_bracket:
-        del list_[mid_l]
-        del list_[mid_r]
-        mid_l -= 1
-        mid_l += 1
-    else:
-        mid_r += 1
+# for _ in range(len(list_)//2):
+#     # for bracket in list_:
+#     # print("In While loop")
+    
+#     left_bracket = list_[mid_l]
+#     # print(f"left bracket is {left_bracket}")
+       
+#     if bracket_dict.get(left_bracket):
+#         r_bracket = list_[mid_r]
+#         right_bracket = bracket_dict.get(left_bracket)
 
-    if len(list_)==0:
-        print(True)
-        break
+#         # print(f"left bracket is {left_bracket}")
+#         print(f"r bracket is {r_bracket}")
+        
+#         print(f"right bracket is {right_bracket}")
+
+
+#         if r_bracket == right_bracket:
+#             print("equal")
+#             print(f"r bracket is {r_bracket}")
+        
+#             print(f"right bracket is {right_bracket}")
+
+#             del list_[mid_l]
+#             del list_[mid_r]
+#             mid_l -= 1
+#             mid_l += 1
+#             print(list_)
+#         else:
+#             mid_r += 1
+
+#         if len(list_)==0:
+#             print(True)
+#             break
+#     else:
+#         mid_l -= 1
     
 
